74bcf9934031a53989bb682c7255bb27b7f1f1fc
Michael Ondek <mondek21@gmail.com>
2020-08-25 20:42:33 -0500

    0-starting-server-side

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,8 @@ import { /*getTemp,*/ postData, updateUI, performAction } from './js/app'
 
 import './styles/style.scss';
 
-
+//use addEventListener to call exported fucntions
+//document.getElementById('btn-add').addEventListener('click', addHandleSubmit.addHandleSubmit);
+//document.getElementById('btn-init').addEventListener('click', addHandleSubmit.getList);
 
 export { /*getTemp,*/ postData, updateUI, performAction }
