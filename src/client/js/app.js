81bed08022993eb486d7d6f74715283a63c76c7c
Michael Ondek <mondek21@gmail.com>
2020-08-26 21:29:49 -0500

    fully_functional

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,19 +1,10 @@
-/* Global Variables */
-//let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 let zipInput = document.getElementById('zip');
 let dateInput = document.getElementById('feelings');
 
-//const key = process.env.w_app_key;
-//console.log(key);
-// Create a new date instance dynamically with JS
 let d = new Date();
 let todayDate = (d.getMonth()+1)+'/'+ d.getDate()+'/'+ d.getFullYear();
 
-console.log(todayDate);
-
-//console.log(milDiff);
-/* Function to GET Web API Data*/
 /* Function to POST data */
 const postData = async(path, data) => {
   console.log('postData')
@@ -26,59 +17,66 @@ const postData = async(path, data) => {
     body: JSON.stringify(data),
   })
 }
-/*update UI function*/
+
 const updateUI = async() => {
 
   const response = await fetch('/all');
   console.log('updateUI');
   console.log(response);
   try {
-  //const allData = input;
-  const allData = await response.json();
-  console.log(allData);
-  console.log('updating UI');
-  document.getElementById('date').innerHTML = `<span class="entry-item">Date: </span>${allData.date}`
-  document.getElementById('temp').innerHTML = `<span class="entry-item">Temperature: </span>${allData.temp}`
-  document.getElementById('content').innerHTML = `<span class="entry-item">You feel: </span>${allData.userRes}`
+    const allData = await response.json();
+    console.log(allData);
+    console.log('updating UI');
+    document.getElementById('date').innerHTML = `<span class="entry-item">Plan: </span>${allData.date}`
+    document.getElementById('temp').innerHTML = `<span class="entry-item">Forecast: </span>${allData.temp}`
+    document.getElementById('content').innerHTML = `<span class="entry-item">Destination: </span>${allData.userRes}`
+    document.getElementById('pic').innerHTML = `<span class="entry-item"><img src=${allData.picture} width="50%" </span>`
   }
   catch(error) {
     console.log('error', error);
   }
 }
-/*post project data*/
 const performAction = async function() {
   console.log('perform action')
-/*add zip data to post*/
   const zipCodeInput = {
     zip: zipInput.value
   }
   await postData('/zip', zipCodeInput)
   let thenDate = dateInput.value;
-  //let dateDifference = thenDate-todayDate;
-  //console.log(thenDate);
-  //console.log(todayDate);
-  //console.log(dateDifference);
-  //let milToday = todayDate.getTime();
   let date1 = new Date(todayDate);
   let date2 = new Date(thenDate);
   let date3 = (date2 - date1)/(60*60*24*1000);
+  console.log(`date2 = ${date2}`)
+
+  let myMonth = (date2.getMonth()+1);
+  let month = (`0${myMonth}`).slice(-2);
+  let myDay = (date2.getDate()+1);
+  let day = (`0${myDay}`).slice(-2);
+  console.log(`month = ${month}`)
+
+  console.log(`day = ${day}`);
+  let mmdd = (`${month}-${day}`);
+  console.log(mmdd);
+  console.log(`date input ${dateInput.value}`);
+  /*let trippyDate = thenDate[0,2];
+  console.log(`trippyDate ${trippyDate}`)*/
   console.log(date3);
-  //let milThen = thenDate.getTime();
-  //let milDiff = milThen - milToday;
   console.log('webTemp')
 
   const data = {
-    //temp: tempFromServer,
-    date: todayDate,
+    monthanddate: mmdd,
+    tripLength: date3,
+    tripDate: date2,
+    date: date1,
     userRes: feelings.value
   }
   console.log(data);
-    /*add data to Post*/
+
   await postData('/add', data)
-    //update UI
+
   updateUI()
 }
 /*Add event listener*/
 document.getElementById('generate').addEventListener('click', performAction);
 
-export { /*getTemp,*/ postData, updateUI, performAction }
+export { postData, updateUI, performAction }
--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,20 @@
-//const dotenv = require('dotenv');
-//dotenv.config({path: '../.env'});
+
 const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
 
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 let geourl = 'http://api.geonames.org/searchJSON?q=';
 let weatherurl = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=';
+let weatherurl2 = 'https://api.weatherbit.io/v2.0/normals?lat=';
 let pixurl = 'https://pixabay.com/api/?';
 
-//const key = 'd8c4b432924994fcf9466fa03ed7d8e1&units=imperial';
 const key = process.env.owkey
 const weatherBitKey = process.env.weather_bit_key
 const geouser = process.env.geou;
 const pix_key = process.env.pixy_key;
 const bodyParser = require('body-parser');
-const  cors = require('cors');
+const cors = require('cors');
 const app = express()
 const port = 8081
 const fetch = require('node-fetch')
@@ -43,21 +41,31 @@ app.listen(port, function () {
 
 app.post('/add', async(req, res) => {
   try {
-  projectData.date = req.body.date;
-  projectData.userRes = req.body.userRes;
-  projectData.temp = await getLatLon(zipData.zip)
+  const theLocation = await getLatLon(zipData.zip);
+  console.log(theLocation);
+  let lat = theLocation[0];
+  let lon = theLocation[1];
+  let country = theLocation[2];
+  picData = await getPics(zipData.zip);
+  console.log(`trip length of ${req.body.triplength}`);
+  console.log(`mmdd = ${req.body.monthanddate}`)
+  const theWeather = await getWeather(lat, lon, req.body.tripLength, req.body.monthanddate);
+  console.log(`The weather report shows:  ${theWeather}`);
+
+  projectData.monthDate = req.body.monthanddate;
+  projectData.length = req.body.tripLength;
+  projectData.today = req.body.date;
+  projectData.date = `Your trip starts ${req.body.userRes}... in ${req.body.tripLength} days`;
+  projectData.userRes = `Beautiful ${zipData.zip}, ${country}`;
+  projectData.temp = `${theWeather[2]}, High of ${theWeather[0]}, Low of ${theWeather[1]}`;
+  projectData.city = zipData.zip;
+  projectData.country = country;
+  projectData.picture = picData;
 
   res.send();
   console.log('app.post/add run')
   console.log(projectData);
 
-  const theLocation = await getLatLon(zipData.zip);
-  console.log(theLocation);
-  let lat = theLocation[0];
-  let lon = theLocation[1];
-  const theWeather = await getWeather(lat, lon)
-  console.log(`The weather report shows:  ${theWeather}`);
-  picData = await getPics(zipData.zip);
   console.log(`The pics are at: ${picData}`);
 
   } catch(e) {
@@ -71,7 +79,6 @@ app.post('/zip', async(req, res) => {
   console.log(zipData);
   res.send();
   console.log(zipData.zip);
-  //tempData = getTemperature(zipData.zip);
   cityData = await getLatLon(zipData.zip);
   picData = await getPics(zipData.zip);
   console.log('app.post/zip run');
@@ -84,20 +91,8 @@ app.post('/zip', async(req, res) => {
   }
 })
 
-const getTemperature = async(zip)=>{
-  const url =  `${baseURL}${zip}&appid=${key}`;
-  const response = await fetch(url);
-  let data = await response.json();
-  let fullTempData = data.main.temp;
-
-  console.log(fullTempData);
-  console.log('getTemp run');
-  return fullTempData;
-}
-
 const getLatLon = async(city)=>{
   console.log('getLatLon');
-  //const url = `${geourl}${city}&maxrows=1&username=${geouser}`;
   const url = `${geourl}${city}&maxRows=1&username=${geouser}`;
   const response = await fetch(url);
   let data = await response.json();
@@ -112,17 +107,33 @@ const getLatLon = async(city)=>{
   return coordinates;
 }
 
-const getWeather = async(lat, lon)=>{
+const getWeather = async(lat, lon, tMinus, hDate)=>{
   console.log('getWeather');
-  const url = `${weatherurl}${lat}&lon=${lon}&units=i&key=${weatherBitKey}`;
-  const response = await fetch(url);
-  let data = await response.json();
-  const high = data.data[0].max_temp;
-  const low = data.data[0].low_temp;
-  const weather = data.data[0].weather.description;
-  const weatherData = [high, low, weather];
-  console.log(weatherData);
-  return weather;
+  if (tMinus < 16) {
+    let url = `${weatherurl}${lat}&lon=${lon}&units=i&key=${weatherBitKey}`;
+    const response = await fetch(url);
+    let data = await response.json();
+    const high = data.data[tMinus].max_temp;
+    console.log(`before 15 high ${high}`)
+    const low = data.data[tMinus].low_temp;
+    const weather = data.data[tMinus].weather.description;
+    const weatherData = [high, low, weather];
+    console.log(weatherData);
+    return weatherData;
+  } else {
+    let url2 = `${weatherurl2}${lat}&lon=${lon}&units=i&start_day=${hDate}&end_day=${hDate}&tp=daily&key=${weatherBitKey}`;
+    const response = await fetch(url2);
+    let data = await response.json();
+    console.log(`url2 = ${url2}`)
+    console.log(`data after 15 is ${data}`)
+    const high = data.data[0].max_temp;
+    console.log(`after 15 high ${high}`)
+    const low = data.data[0].min_temp;
+    const weather = 'Historically';
+    const weatherData = [high, low, weather];
+    console.log(weatherData);
+    return weatherData;
+  }
 }
 
 const getPics = async(city)=>{
@@ -133,5 +144,4 @@ const getPics = async(city)=>{
   let image_url = data.hits[0].webformatURL;
   console.log(image_url);
   return image_url;
-
 }
